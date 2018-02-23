bdd5ea25912a39e00b76ed771dbcc6417958a343
alfredoPacheco <apacheco@capsonic.com>
2018-02-22 19:09:23 -0700

    -Adding common services.

---
+++ frontend/CA2/app/scripts/services/app-config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ca2App.appConfig
+ * @description
+ * # appConfig
+ * Constant in the ca2App.
+ */
+angular.module('ca2App').constant('appConfig', {
+    API_URL: 'http://localhost:57063/api/',
+    // API_URL: 'http://apps.capsonic.com/www/api/',
+    APP_NAME: 'CA2'
+});
---
+++ frontend/CA2/app/scripts/services/auth-interceptor-service.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ca2App.authInterceptorService
+ * @description
+ * # authInterceptorService
+ * Factory in the ca2App.
+ */
+angular.module('ca2App').factory('authInterceptorService', function($q, $location, localStorageService) {
+    var authInterceptorServiceFactory = {};
+
+    var _request = function(config) {
+
+        config.headers = config.headers || {};
+
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            config.headers.Authorization = 'Bearer ' + authData.token;
+        }
+
+        return config;
+    }
+
+    var _responseError = function(rejection) {
+        if (rejection.status === 401) {
+            localStorageService.remove('authorizationData');
+            $location.path('/login');
+        }
+        return $q.reject(rejection);
+    }
+
+    authInterceptorServiceFactory.request = _request;
+    authInterceptorServiceFactory.responseError = _responseError;
+
+    return authInterceptorServiceFactory;
+});
